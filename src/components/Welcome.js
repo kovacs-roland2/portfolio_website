acab75e6ae08f7f29e8b72c58778c01436399eaa
kovacs-roland2 <koviroli96@gmail.com>
2024-09-14 17:07:13 +0200

    add welcome page style

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,9 +4,12 @@ import './Welcome.css';
 const Welcome = () => {
   return (
     <div className="welcome-container">
-      <h1 className="welcome-title">Welcome!</h1>
-      <h2 className="welcome-name">I am Roland Kovacs</h2>
-      <p className="welcome-role">Data Scientist</p>
+      <h1 className="welcome-title">Hello, I'm Roland.<br />I'm a Data Scientist.</h1>
+      <p className="welcome-description">
+        I'm a Data Scientist graduate with a demonstrated history of working in the information technology and services industry. Familiar with gathering, cleaning, and organizing data for use. 
+        Advanced understanding of statistical, algebraic, and other analytical techniques. Skilled in Python, Machine Learning, Selenium, Data Analysis, and React. 
+        Strong research professional with a Master's degree focused in Business Informatics from Budapest University of Technology and Economics. Interested in sport analytics.
+      </p>
     </div>
   );
 };
