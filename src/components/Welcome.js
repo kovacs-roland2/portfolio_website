0dbf3c21de3b64d5ba35d1b511cbfd0ecfc6cc19
kovacs-roland2 <koviroli96@gmail.com>
2024-09-14 14:45:44 +0200

    welcome page

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Welcome.css';
+
+const Welcome = () => {
+  return (
+    <div className="welcome-container">
+      <h1 className="welcome-title">Welcome!</h1>
+      <h2 className="welcome-name">I am Roland Kovacs</h2>
+      <p className="welcome-role">Data Scientist</p>
+    </div>
+  );
+};
+
+export default Welcome;
