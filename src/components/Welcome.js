3ec2ce949f39f1f9a0a51f3e6a0f54200ee0d56d
kovacs-roland2 <koviroli96@gmail.com>
2024-09-15 14:51:45 +0200

    contact me icons

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -10,6 +10,20 @@ const Welcome = () => {
         Advanced understanding of statistical, algebraic, and other analytical techniques. Skilled in Python, Machine Learning, Selenium, Data Analysis, and React. 
         Strong research professional with a Master's degree focused in Business Informatics from Budapest University of Technology and Economics. Interested in sport analytics.
       </p>
+      <div className="contact-section">
+        <a href="https://github.com/kovacs-roland2" target="_blank" rel="noopener noreferrer" className="contact-link">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/roland-kovacs96/" target="_blank" rel="noopener noreferrer" className="contact-link">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="https://medium.com/@rolandkovcs_38012" target="_blank" rel="noopener noreferrer" className="contact-link">
+          <i className="fab fa-medium"></i>
+        </a>
+        <a href="mailto:koviroli96@gmail.com" className="contact-link">
+          <i className="fas fa-envelope"></i>
+        </a>
+      </div>
     </div>
   );
 };
