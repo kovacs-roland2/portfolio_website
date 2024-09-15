d06afd035c7883678d8ee4a3a5a4b86b5aeddc8a
kovacs-roland2 <koviroli96@gmail.com>
2024-09-15 17:11:14 +0200

    img to welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,30 +1,40 @@
-import React from 'react';
 import './Welcome.css';
+import Header from './Header';
 
 const Welcome = () => {
   return (
-    <div className="welcome-container">
-      <h1 className="welcome-title">Hello, I'm Roland.<br />I'm a Data Scientist.</h1>
-      <p className="welcome-description">
-        I'm a Data Scientist graduate with a demonstrated history of working in the information technology and services industry. Familiar with gathering, cleaning, and organizing data for use. 
-        Advanced understanding of statistical, algebraic, and other analytical techniques. Skilled in Python, Machine Learning, Selenium, Data Analysis, and React. 
-        Strong research professional with a Master's degree focused in Business Informatics from Budapest University of Technology and Economics. Interested in sport analytics.
-      </p>
-      <div className="contact-section">
-        <a href="https://github.com/kovacs-roland2" target="_blank" rel="noopener noreferrer" className="contact-link">
-          <i className="fab fa-github"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/roland-kovacs96/" target="_blank" rel="noopener noreferrer" className="contact-link">
-          <i className="fab fa-linkedin"></i>
-        </a>
-        <a href="https://medium.com/@rolandkovcs_38012" target="_blank" rel="noopener noreferrer" className="contact-link">
-          <i className="fab fa-medium"></i>
-        </a>
-        <a href="mailto:koviroli96@gmail.com" className="contact-link">
-          <i className="fas fa-envelope"></i>
-        </a>
+    <>
+      <Header/>
+      <div className='welcome-wrapper'>
+        <div className="welcome-container">
+          <div className='text-content'>
+            <h1 className="welcome-title">Hello, I am Roland.</h1>
+            <p className="welcome-description">
+            I'm a Data Scientist with a strong background in information technology and services. I specialize in gathering, cleaning, and analyzing data using 
+            advanced statistical and machine learning techniques. With skills in Python, Machine Learning, Data Analysis, and React, I bring technical expertise to 
+            solving complex data problems. I hold a Master's degree in Business Informatics from Budapest University of Technology and Economics and have a keen 
+            interest in football analytics.
+            </p>
+            <div className="contact-section">
+              <a href="https://github.com/kovacs-roland2" target="_blank" rel="noopener noreferrer" className="contact-link">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/roland-kovacs96/" target="_blank" rel="noopener noreferrer" className="contact-link">
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a href="https://medium.com/@rolandkovcs_38012" target="_blank" rel="noopener noreferrer" className="contact-link">
+                <i className="fab fa-medium"></i>
+              </a>
+              <a href="mailto:koviroli96@gmail.com" className="contact-link">
+                <i className="fas fa-envelope"></i>
+              </a>
+            </div>
+          </div>
+          <img src="./photo.jpg" alt="Roland Kovacs" className="profile-photo" />
+        </div>
       </div>
-    </div>
+    </>
+  
   );
 };
 
