72507f40d73d89e44991c36e82eff8f3d41b0733
roland-kovacs <56347547+kovacs-roland2@users.noreply.github.com>
2025-06-19 13:44:39 +0200

    about changes

---
+++ src/content/textContent.js
@@ -0,0 +1,42 @@
+export const about_me = `I'm Roland Kovacs, a Data Scientist with a passion for turning raw data into actionable insights. 
+I got a Master's degree in Business Informatics from Budapest University of Technology and Economics, 
+with a specialization in using machine learning, data analysis and statistical techniques to solve complex problems. 
+I have experience working at IBM where I built predictive models, developed web applications, 
+and applied Natural Language Processing to optimize user experience. I'm comfortable in Python, Machine Learning, 
+React, and Data Visualization tools. In addition to technical expertise, I also have a strong interest in football analytics. 
+I have written research papers on player transfers and career development. When I'm not working with data 
+I also enjoy playing football, hiking, and cycling.`;
+
+export const tools = [
+    { key: 'Programing languages:', values: "Python | R | SQL" }, 
+    { key: 'Web development:', values: "JavaScript | React | Next.js | TypeScript" },
+    { key: 'Frameworks:', values: "Django | FastAPI | scikit-learn | Tensorflow | Pytorch | LangChain" }, 
+    { key: 'Data visualization:', values: "Tableau | PowerBI" },
+    { key: 'Tools/Platforms:', values: "AWS | Git | Docker | CI/CD" },
+  ];
+
+export const experience = [
+    {
+        company: "IBM",
+        location: "Budapest, Hungary",
+        position: "Data Scientist",
+        duration: "Apr 2022 - Apr 2025",
+        description: ["Developed predictive machine learning model to forecast customer decisions at the end of their lease or loan lifecycle, improving revenue forecasting accuracy by 15%.",
+            "Implemented clustering model to analyze sales opportunities, uncovering patterns in client behavior and enabling prioritization of high-value deals.",
+            "Created and maintained NLP models and pipelines to analyze internal ticketing data, identifying root causes of recurring system issues and achieving a 10% year-to-date reduction in ticket volume.",
+            "Built a Generative AI assistant using Retrieval-Augmented Generation (RAG) and LLMs, improving analysts' productivity and accelerating access to internal knowledge bases.",
+            "Delivered presentations and reports to global stakeholders, translating complex data into actionable insights and raising awareness of system bottlenecks through analysis of 1,000+ error messages.",
+        ]
+    },
+    {
+        company: "IBM",
+        location: "Budapest, Hungary",
+        position: "Business Analyst",
+        duration: "Jun 2018 - Apr 2022",
+        description: ["Developed interactive web application frontends and implemented image-based search functionality, resulting in a 35% increase in active user engagement.",
+            "Developed financial SQL queries for audit, ensuring accuracy and compliance with regulatory standards.",
+            "Collaborated closely with cross-functional teams and stakeholders to interpret and resolve customer-reported incidents, effectively communicated findings, and delivered solutions to improve user experience.",
+            "Automated data migration workflows using Python and Selenium, eliminating 100+ hours of manual work.",
+        ]
+    }
+]
