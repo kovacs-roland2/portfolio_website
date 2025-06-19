391bc889b7d18a66ed99cab994bacd075df083b4
roland-kovacs <56347547+kovacs-roland2@users.noreply.github.com>
2025-06-19 16:30:17 +0200

    header scroll fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -20,7 +20,7 @@ const navItems: NavItem[] = [
 
 const Header = () => {
   return (
-    <NavigationMenu className="fixed top-8 left-1/2 transform -translate-x-1/2 z-[100] bg-transparent">
+    <NavigationMenu className="absolute top-8 left-1/2 transform -translate-x-1/2 z-[100] bg-transparent">
       <NavigationMenuList className="flex gap-4 p-2">
         {navItems.map(item => (
           <NavigationMenuItem key={item.href}>
